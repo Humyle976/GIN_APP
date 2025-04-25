7004490d8ba412064a69eeb17814e82a68b8febe
Humyle976 <shumylem21@gmail.com>
2025-04-25 18:28:13 +0500

    Setup basic gin app

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, gin.H{
+			"message": "Hello, Gin!",
+		})
+	})
+
+	err := router.Run(":8000")
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
+}
