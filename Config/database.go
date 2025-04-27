7bccf915c4098cf89508397072caff6fb7a3cf37
Humyle976 <shumylem21@gmail.com>
2025-04-27 23:25:13 +0500

    Added database connection

---
+++ Config/database.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := os.Getenv("DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Couldn't connect to the database: ", err)
+	} else {
+		log.Println("Connected To The Database")
+	}
+	DB = db
+}
